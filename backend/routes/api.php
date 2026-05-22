71c28a34eb2a55f4d5ea3221f2ee9b5852fca120
fatiya17 <fatiyalabibah17@gmail.com>
2026-05-22 18:07:58 +0700

    feat: add user profile management routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
@@ -15,5 +16,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::get('/auth/me', [AuthController::class, 'me']);
 
-    // 
+    // user profile
+    Route::prefix('user')->group(function () {
+        Route::get('/profile', [UserController::class, 'show']);
+        Route::put('/profile', [UserController::class, 'update']);
+        Route::put('/password', [UserController::class, 'changePassword']);
+        Route::post('/photo', [UserController::class, 'uploadPhoto']);
+        Route::delete('/photo', [UserController::class, 'deletePhoto']);
+    });
 });
