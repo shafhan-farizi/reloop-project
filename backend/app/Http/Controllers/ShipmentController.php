704ca1432fc7245d000872e9dd4ff66318d9f7ff
shafhan <shafhan.fa@gmail.com>
2026-05-31 14:56:35 +0100

    chore: resolve sync conflict on main branch

--- backend/app/Http/Controllers/ShipmentController.php
+++ backend/app/Http/Controllers/ShipmentController.php
@@ -17,15 +17,11 @@ use Illuminate\Support\Facades\Validator;
 
 class ShipmentController extends Controller
 {
-<<<<<<< HEAD
-    public function __construct(protected FileUploadService $uploadService) {}
-=======
     // Inject FileUploadService dan NotificationService melalui constructor
     public function __construct(
         protected FileUploadService   $uploadService,
         protected NotificationService $notifService,
     ) {}
->>>>>>> chore/faker-id
 
     /**
      * GET /api/shipments
@@ -440,41 +436,4 @@ class ShipmentController extends Controller
             ]
         ], 200);
     }
-<<<<<<< HEAD
-
-    /**
-     * GET /api/shipments/{receipt}/track
-     * Lacak resi murni LOKAL MOCKING
-     */
-    public function trackByReceipt(string $receipt): JsonResponse
-    {
-        $validated = Validator::make(['receipt' => $receipt], [
-            'receipt' => ['required', 'string', 'alpha_num']
-        ])->validate();
-
-        
-        $shipment = Shipment::with(['request.item', 'request.item.donor', 'request.requester'])->where('tracking_number', $validated['receipt'])->first();
-
-        return response()->json([
-            'code'    => 200,
-            'status'  => 'success',
-            'data'    => [
-                'shipment' => new ShipmentTrackingResource($shipment),
-            ]
-        ], 200);
-    }
-
-    /**
-     * Helper internal untuk format error response
-     */
-    private function errorResponse(string $message, int $code): JsonResponse
-    {
-        return response()->json([
-            'code'    => $code,
-            'status'  => 'error',
-            'message' => $message,
-        ], $code);
-    }
-=======
->>>>>>> chore/faker-id
 }
